a6b01cae90c8caec9ce787bf27f678f2bb336193
Erlanggabayu4293 <erlanggabayu03@students.amikom.ac.id>
2022-06-12 19:49:47 +0700

    initial commit

---
+++ Canon.cs
@@ -0,0 +1,13 @@
+﻿namespace LatihanPolymorphism
+{
+    class Canon : PrinterWindows
+    {
+        internal override void Show() {
+            Console.WriteLine("Canon Display dimension : 9.5*12");
+        }
+
+        internal override void Print() {
+            Console.WriteLine("Canon pirnter printing..");
+        }
+    }
+}
